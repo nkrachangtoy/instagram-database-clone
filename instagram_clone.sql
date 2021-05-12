e89f77ca3ce7f4cf62a0bac347a94cb436b5f43f
Nusorn <nkrachangtoy@gmail.com>
2021-05-11 20:59:46 -0700

    create comments table

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -36,7 +36,18 @@ CREATE TABLE photos
 	image_url VARCHAR(255) NOT NULL,
 	user_id INT NOT NULL,
 		FOREIGN KEY(user_id) REFERENCES users(id),
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments
+(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INT NOT NULL,
+		FOREIGN KEY(user_id) REFERENCES users(id),
+	photo_id INT NOT NULL,
+		FOREIGN KEY(photo_id) REFERENCES photos(id),
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 -- INSERT users
@@ -44,6 +55,9 @@ INSERT INTO users (username) VALUES ('bambam'),('mark'),('jackson');
 
 -- INSERT photo
 INSERT INTO photos (image_url, user_id) VALUES ('/asdfsadf',1),('/afdfdf',2),('/sdasaf',3)
+
+-- INSERT comments
+INSERT INTO comments (comment_text, user_id, photo_id) VALUES ('Good shot!', 1, 2),('Nice photo!', 3, 2),('Hi Mark!', 3, 2);
 
 
 
