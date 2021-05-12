3c4ce506fc4ef43f7c7187d1eaeae8826db6809f
Nusorn <nkrachangtoy@gmail.com>
2021-05-11 23:35:20 -0700

    Change to MySQL syntax

--- instagram_clone.sql
+++ instagram_clone.sql
@@ -6,29 +6,28 @@ USE ig_clone;
 -- Create Tables -- 
 CREATE TABLE users
 (
-	id INT IDENTITY(1,1) PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) UNIQUE NOT NULL,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+	created_at TIMESTAMP DEFAULT NOW()
 
 CREATE TABLE photos
 (
-	id INT IDENTITY(1,1) PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	image_url VARCHAR(255) NOT NULL,
 	user_id INT NOT NULL,
 		FOREIGN KEY(user_id) REFERENCES users(id),
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE comments
 (
-	id INT IDENTITY(1,1) PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	comment_text VARCHAR(255) NOT NULL,
 	user_id INT NOT NULL,
 		FOREIGN KEY(user_id) REFERENCES users(id),
 	photo_id INT NOT NULL,
 		FOREIGN KEY(photo_id) REFERENCES photos(id),
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE likes
@@ -38,7 +37,7 @@ CREATE TABLE likes
 		FOREIGN KEY(user_id) REFERENCES users(id),
 	photo_id INT NOT NULL,
 		FOREIGN KEY(photo_id) REFERENCES photos(id),
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP DEFAULT NOW()
 	-- This will not allow user to insert more than 1 like with the same user_id and photo_id
 	PRIMARY KEY (user_id, photo_id)
 );
@@ -49,15 +48,15 @@ CREATE TABLE follows
 		FOREIGN KEY (follower_id) REFERENCES users(id),
 	followee_id INT NOT NULL,
 		FOREIGN KEY (followee_id) REFERENCES users(id),
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT NOW(),
 	PRIMARY KEY (follower_id, followee_id)
 );
 
 CREATE TABLE tags
 (
-	id INT IDENTITY(1,1) PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	tag_name VARCHAR(255) UNIQUE,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE photo_tags
@@ -76,7 +75,3 @@ CREATE TABLE photo_tags
 
 
 -- SELECT STATEMENTS ------------------
---SELECT photos.image_url, users.username
---FROM photos
---JOIN users	
---	ON photos.user_id = users.id;
